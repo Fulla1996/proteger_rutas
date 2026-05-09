f1f16775d48209f4300e87faa0cb926fc52cc6f5
Fulla1996 <54816344+Fulla1996@users.noreply.github.com>
2026-05-09 02:02:06 -0300

    Se eliminan comentarios y archivos innecesarios

--- src/pages/auth/registro/registro.ts
+++ src/pages/auth/registro/registro.ts
@@ -16,7 +16,6 @@ form.addEventListener('submit', (event) => {
         loggedIn: false,
         role: 'client' // Asignamos un rol por defecto
     };
-    console.log('Usuario registrado:', usuario);
     const users: IUser[] = JSON.parse(localStorage.getItem('users') || '[]'); // Obtenemos el array de usuarios desde localStorage
     if (users.some(user => user.email === usuario.email)) {
         alert('El email ya está registrado. Por favor, utiliza otro email.');
@@ -25,7 +24,6 @@ form.addEventListener('submit', (event) => {
     else {
         users.push(usuario); // Agregamos el nuevo usuario al array
         localStorage.setItem('users', JSON.stringify(users)); // Guardamos el array actualizado en localStorage
-        console.log('Usuarios en localStorage:', localStorage.getItem('users'));
         alert('Registro exitoso. Ahora puedes iniciar sesión.');
         navigate('/src/pages/auth/login/login.html'); // Redirigimos al usuario a la página de login
     }
